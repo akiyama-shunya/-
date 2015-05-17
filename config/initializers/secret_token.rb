a33360f73d9c5ff78aa8bc833e6eb78d53722938
akiyama <akiyama@lictoor.com>
2015-05-17 14:19:00 +0000

    初期設定

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seleclothes::Application.config.secret_key_base = '8d1428bd5931c9bd2ebd274b927b22186364e803e2e7cd37b188216b0f69f3b8691ab874d90fc6739891b960508f2b8180062283ec85478e5777990cd31e4b0e'
